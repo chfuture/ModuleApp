280e953a0dfead6ed7b04eef31f7190dfc931954
ycwang <731651897@qq.com>
2018-08-17 17:53:19 +0800

    ARouter/Reflect Fragment解耦

--- app/src/main/java/com/ycwang/moduleapp/fragment/MyFragment.java
+++ app/src/main/java/com/ycwang/moduleapp/fragment/MyFragment.java
@@ -1,5 +1,7 @@
 package com.ycwang.moduleapp.fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -24,24 +26,87 @@ public class MyFragment extends Fragment {
 
     View view;
 
+
+    private Activity activity;
+
     public static Fragment getInstance() {
         return new MyFragment();
     }
 
+    //Fragment和Activity相关联时调用。可以通过该方法获取Activity引用，还可以通过getArguments()获取参数
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (Activity) context;
+    }
+
+    //Fragment被创建时调用。
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    //创建Fragment的布局。
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_my, container,false);
+        view = inflater.inflate(R.layout.fragment_my, container, false);
         return view;
     }
 
+    //当Activity完成onCreate()时调用。
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         txwFragment = view.findViewById(R.id.txw_fragment);
         txwFragment.setText("Fragment My");
+    }
 
+    //当Fragment可见时调用。
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    //当Fragment可见且可交互时调用。
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    //当Fragment不可交互但可见时调用。
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    //当Fragment不可见时调用。
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 
 
+    //当Fragment的UI从视图结构中移除时调用。
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    //销毁Fragment时调用。
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    //当Fragment和Activity解除关联时调用。
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+
+    //只有onCreateView()在重写时不用写super方法，其他都需要。
 }
